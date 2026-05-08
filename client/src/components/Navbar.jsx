1b58410cad4efc50a5f52ac2fa966f85a63cdf85
ManviSharivastava24 <manvishrivastava0516@gmail.com>
2026-05-08 13:12:44 +0530

    n

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const {user}=useUser()
   const {openSignIn}=useClerk()
   const navigate=useNavigate()
-  const {favoriteMovies}=useAppContext();
+  const {favoriteMovies,isAdmin}=useAppContext();
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
        <Link to='/' className='max-md:flex-1'>
@@ -24,7 +24,7 @@ const Navbar = () => {
               <Link  onClick={()=> {scrollTo(0,0);setIsOpen(false)}} to='/'> Releases</Link> 
                {favoriteMovies.length >0 && <Link onClick={()=> {scrollTo(0,0);setIsOpen(false)}} to='/favorites'> Favorites</Link> }
        </div>
-       <div className='flex items-center gap-8'>
+       <div className='flex items-center gap-4'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
         {
           !user ? (
@@ -33,6 +33,7 @@ const Navbar = () => {
 
             <UserButton >
               <UserButton.MenuItems>
+                {isAdmin && <UserButton.Action label="Admin Dashboard" labelIcon={<TicketPlus width={15}/>} onClick={() => navigate('/admin')} />}
                 <UserButton.Action label="My Bookings" labelIcon={<TicketPlus width={15}/>} onClick={() => navigate('/my-bookings')} />
               </UserButton.MenuItems>
               </UserButton>
